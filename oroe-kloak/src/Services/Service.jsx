989c233dd802f8a1efafa2de7bfa8b4871e683ac
Aaron Moses <pastures77@gmail.com>
2024-06-29 10:50:39 +0200

    feat: Update Service component with new images for improved visual appeal

--- oroe-kloak/src/Services/Service.jsx
+++ oroe-kloak/src/Services/Service.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Slider from 'react-slick';
-import Group91 from '../images/Group-91.png';
+import IMG7 from '../images/IMG-7.jpg';
+import IMG8 from '../images/IMG-8.jpg';
 import './Service.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -19,15 +20,15 @@ export default function Service() {
       <div className="service-info-container">
         <h1 className="service-title">Our Services</h1>
         <Slider {...settings}>
-          <div className='service-image'>
-            <img src={Group91} alt="Group-91" />
+          <div className="service-image">
+            <img src={IMG7} alt="IMG-7" />
             <div></div>
           </div>
           {/* <div>
             <img src={Niels} alt="Niels-1" />
           </div> */}
           <div>
-            <img src={Group91} alt="Service 3" />
+            <img src={IMG8} alt="IMG-8." />
           </div>
         </Slider>
       </div>
